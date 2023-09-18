b2f3a9a89b1e36d1d5212758173f6ec3ae1fa85e
rukalchau <rukalchau@gmail.com>
2023-09-19 01:34:00 +0200

    rename folder & done FP ex1

--- FP/lab1/EX1_sumPrimesTo/sumPrimeV2.hs
+++ FP/lab1/EX1_sumPrimesTo/sumPrimeV2.hs
@@ -3,9 +3,14 @@
 isPrime :: Integer -> Bool
 isPrime 1 = False
 isPrime 2 = True
-isPrime n = not(null [i|i<-[3,5..n-1],n `mod` i ==0])
+isPrime n = check 2
+    where check f | f^2>n = True
+                  | rem n f ==0 = False
+                  | otherwise = check (f+1)
+
 
 sumPrimesTo :: Integer -> Integer
+sumPrimesTo 0 = 0
 sumPrimesTo 1 = 0
 sumPrimesTo 2 = 2
 sumPrimesTo n | isPrime n = n+sumPrimesTo (n-1)
