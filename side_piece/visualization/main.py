4f33dc62af36518ef2d52dced54f325d4b9453a5
rukalchau <rukalchau@gmail.com>
2023-11-08 17:30:09 +0100

    update

--- side_piece/visualization/main.py
+++ side_piece/visualization/main.py
@@ -2,49 +2,9 @@ from datetime import datetime
 from chat2 import convert
 from plotter_v2 import mylt
 
-data = """2/11/2023, 11:36:35 AM GMT+1$IS
+my_file = open("studylog.txt", "r")
+data=(my_file.read())
 
-2/11/2023, 1:44:24 PM GMT+1$IS
-
-2/11/2023, 7:13:21 PM GMT+1$IS
-
-3/11/2023, 4:31:23 AM GMT+1$IS
-
-3/11/2023, 6:11:54 AM GMT+1$IS
-
-3/11/2023, 6:46:59 AM GMT+1$IS
-
-3/11/2023, 7:19:10 AM GMT+1$IS
-
-3/11/2023, 8:21:18 AM GMT+1$IS
-
-3/11/2023, 9:54:53 AM GMT+1$IS
-
-4/11/2023, 7:46:35 AM GMT+1$Python
-
-4/11/2023, 10:10:42 AM GMT+1$Python
-
-4/11/2023, 2:31:08 PM GMT+1$Python
-
-4/11/2023, 3:01:51 PM GMT+1$Python
-
-5/11/2023, 7:57:58 AM GMT+1$Python
-
-5/11/2023, 3:24:54 PM GMT+1$Python
-
-5/11/2023, 3:59:39 PM GMT+1$Python
-
-7/11/2023, 9:29:12 AM GMT+1$Python
-
-7/11/2023, 5:40:06 PM GMT+1$Convex OPT
-
-7/11/2023, 6:13:30 PM GMT+1$Convex OPT
-
-7/11/2023, 6:54:53 PM GMT+1$Convex OPT
-
-7/11/2023, 11:03:08 PM GMT+1$Convex OPT
-
-7/11/2023, 11:37:53 PM GMT+1$Convex OPT"""
 
 # Split the data into lines
 lines = data.split('\n')
